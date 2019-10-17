ee0240a8808ee9c64bb08ec4039544d738b5752f
John Delong <jdelong15@hotmail.ca>
2019-10-17 16:34:32 -0400

    test

--- contact/email.php
+++ contact/email.php
@@ -1,15 +1,14 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "jdelong11@outlook.com";
-    $from = $_POST['email']; 
-    $name = $_POST['name'];
-
-    $subject = "Job Opening";
-    $subject2 = "Copy of your form submission";
-    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from;
-    mail($to,$subject,$message,$headers);
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    }
+<?php
+$to = 'jdelong11@outlook.com';
+$subject = 'Marriage Proposal';
+$message = 'Hi Jane, will you marry me?'; 
+$from = 'peterparker@email.com';
+ 
+// Sending email
+if(mail($to, $subject, $message)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
+}
 ?>
+
